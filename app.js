94a29b858078b5e1e8aa95fff47863d41b211f17
Gc87J <91073870+Gc87J@users.noreply.github.com>
2022-03-16 16:07:18 -0500

    Trying to figure out this layout thing with grid

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+/*Variables*/
+const input = document.querySelector("input")
+
+const submitBtn = document.querySelector(".submit-button");
+
+const errorMsg = document.querySelector(".error-msg");
+
+
+const testing = document.querySelector(".testing");
+
+const Para = document.createElement("p");
+
+testing.appendChild(Para)
+
+
+submitBtn.addEventListener("click", ()=> {
+
+
+  errorMsg.style.display = "block"
+
+
+  
+}
+
+)
